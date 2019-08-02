25aff158140d0a8f4723b66fc3c0dd96efd39c91
unknown <rosebengco@gmail.com>
2019-08-02 11:50:09 +0800

    initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 10:34:34 2019
+
+@author: arriba
+"""
+
+for a in range (6):
+       
+    for b in range (a):
+    print ('*')
+    
+    
+Y = int(input) 
